c55b0270066a54cefab4db3ac1004e84da957105
Michael Jarvis <michael@jarvis.com>
2022-10-20 12:04:09 -0500

    Tweak precision, some formatting

--- pwe.c
+++ pwe.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <mpfr.h>
 
+#define BLKC "\e[1;30m"
 #define REDC "\e[31m"
 #define OFFC "\e[0m"
 
@@ -12,11 +13,14 @@ usage(const char *pName)
     printf("Usage: %s <n> <l>\n\n", pName);
     puts("n:\tnumber of unique characters");
     puts("l:\tlength of password");
-    puts("");
+
+    puts(BLKC);
     printf("MPFR library: %-12s\nMPFR header:  %s (based on %d.%d.%d)\n",
-            mpfr_get_version (), MPFR_VERSION_STRING, MPFR_VERSION_MAJOR,
-            MPFR_VERSION_MINOR, MPFR_VERSION_PATCHLEVEL);
-    printf("MPFR_PREC_MIN=%d and MPFR_PREC_MAX=%ld\n", MPFR_PREC_MIN,MPFR_PREC_MAX);
+           mpfr_get_version(), MPFR_VERSION_STRING, MPFR_VERSION_MAJOR,
+           MPFR_VERSION_MINOR, MPFR_VERSION_PATCHLEVEL);
+    printf("MPFR_PREC_MIN=%d and MPFR_PREC_MAX=%ld\n", MPFR_PREC_MIN, MPFR_PREC_MAX);
+    mpfr_prec_t defPrec = mpfr_get_default_prec();
+    mpfr_printf("Default precision is %Pu" OFFC "\n", defPrec);
 }
 
 // You can calculate password entropy by log2( n ** l )
@@ -38,25 +42,25 @@ int main(int argc, char *argv[])
     }
 
     int r;
-    char *ep=NULL;
-    if ((r=mpfr_strtofr(n, argv[1], &ep, 10, MPFR_RNDN)) != 0) {
+    char *ep = NULL;
+    if ((r = mpfr_strtofr(n, argv[1], &ep, 10, MPFR_RNDN)) != 0) {
         fprintf(stderr, REDC "Problem setting n=%s (returned %d)\n" OFFC, argv[1], r);
         return EXIT_FAILURE;
     }
 
-    if ((r=mpfr_strtofr(l, argv[2], &ep, 10, MPFR_RNDN)) != 0) {
+    if ((r = mpfr_strtofr(l, argv[2], &ep, 10, MPFR_RNDN)) != 0) {
         fprintf(stderr, REDC "Problem setting l=%s (returned %d)\n" OFFC, argv[2], r);
         return EXIT_FAILURE;
     }
 
 
-    if ((r=mpfr_pow(powValue, n, l, MPFR_RNDN)) != 0) {
+    if ((r = mpfr_pow(powValue, n, l, MPFR_RNDN)) != 0) {
         mpfr_fprintf(stderr, REDC "mpfr_pow(powValue, %.0RNf, %.0RNf, MPFR_RNDN) returned %d\n" OFFC, n, l, r);
         return EXIT_FAILURE;
     }
 
     mpfr_log2(log2Result, powValue, MPFR_RNDN);
-    mpfr_printf( "password entropy for n=%.0RNf l=%.0RNf is %.RNf\n", n, l, log2Result);
+    mpfr_printf("password entropy for n=%.0RNf l=%.0RNf is %.RNf\n", n, l, log2Result);
 
     return EXIT_SUCCESS;
 }
